d3ef8aa79ac954323fd05ea5c6f8fd6b4b028bdd
Rewa101 <mrbrianrewa@gmail.com>
2024-08-24 11:34:07 +0300

    "PythonAssignment"

---
+++ Assignment.py
@@ -0,0 +1,32 @@
+#Part 1:Variables and Print Function
+#Create Variables
+name="Brian Rewa"
+age=17
+favorite_colour="Blue"
+city="Nairobi"
+#Print Statements
+print(name)
+print(age)
+print(favorite_colour)
+print(city)
+print(f"My name is {name},I am {age} years old,I like {favorite_colour} and I live in {city}",)
+#Part 2:Arithmetic Operations with variables
+#Numeric variables
+a=5
+b=10
+print(a+b)
+print(b-a)
+print(a*b)
+print(b/a)
+#Updating Variables
+c=17
+f=17 + 5
+print(f)
+#Part 3:Challenge section
+#Complex Sentence
+q="No pain no gain"
+print(f"{q},by Brian Rewa.")
+#Personalized Greeting
+n="Freddy"
+print(f"Hello {n} from {name}.")
+
